c70b53cdd0447d5374dd2bb1561d7cf9ac343b34
Arthur Nguyen <141199111+arthurnnguyen@users.noreply.github.com>
2024-05-14 00:48:29 -0700

    Update chat_server.c
    chat_server command argument: ./chat_server 8080

--- chat_server.c
+++ chat_server.c
@@ -15,32 +15,53 @@
 int client_sockets[MAX_CLIENTS] = {0};
 pthread_mutex_t lock;
 
+// Report error function
+void report(const char* msg, int terminate) {
+    perror(msg);
+    if (terminate) exit(-1); /* failure */
+}
+
+// function to handle client messages
 void *client_handler(void *socket_desc) {
+    
+    // Initialize variables
     int new_sock = *(int*)socket_desc;
-    free(socket_desc);
     char client_message[BUFFER_SIZE];
 
     // Receive messages from the client
     while (1) {
         memset(client_message, 0, sizeof(client_message));
-        int read_size = recv(sock, client_message, BUFFER_SIZE, 0);
+        int read_size = recv(new_sock, client_message, BUFFER_SIZE, 0);
         if (read_size <= 0) {
-            break;
+            if (read_size == 0) {
+	        report("Client disconnected", 1);
+	        }
+	        else {
+	    	perror("recv failed");
+	        }
+	        break;
         }
 
         // Write the received message to chat_history
         pthread_mutex_lock(&lock);
         FILE *file = fopen("chat_history", "a");
         if (file) {
-            fprintf(file, "%s\n", client_message);
+            if (fprintf(file, "%s\n", client_message) < 0) {
+	            perror("Faied to write to file");
+	        }
             fclose(file);
         }
-        pthread_mutex_unlock(&lock);
+	    else {
+	        perror("Failed to open file");
+	    }
+	    pthread_mutex_unlock(&lock);
 
         // Send the message back to all clients
         for (int i = 0; i < MAX_CLIENTS; i++) {
             if (client_sockets[i] != 0) {
-                send(client_sockets[i], client_message, strlen(client_message), 0);
+                if (send(client_sockets[i], client_message, strlen(client_message), 0) < 0) {
+	                perror("Failed to send message to client"); 
+		        }   
             }
         }
     }
@@ -48,54 +69,68 @@ void *client_handler(void *socket_desc) {
     // For removing the disconnected client
     pthread_mutex_lock(&lock);
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] == sock) {
+        if (client_sockets[i] == new_sock) {
             client_sockets[i] = 0;
             break;
         }
     }
     pthread_mutex_unlock(&lock);
-    close(sock);
-    return 0;
+    close(new_sock);
+    return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    int server_socket, c, *new_sock;
-    struct sockaddr_in server, client;
 
+// main function
+int main(int argc, char *argv[]) {
+    // Checks if the commnad arguments are correct
     if (argc < 2) {
         printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
+        report("Not enough arguments", 1);
     }
 
     // Create server socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
-        perror("Could not create socket");
-        return 1;
+        report("Could not create socket", 1);
     }
 
-    // Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8080);  // Use the port number passed as an argument
+    // Prepare the server address structure
+    struct sockaddr_in saddr;  // server address
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(atoi(argv[1]));  // Use the port number passed as an argument
 
     // Bind
-    if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Bind failed");
-        return 1;
+    if (bind(server_socket, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
+        report("Bind failed", 1);
     }
 
     // Listen
-    listen(server_socket, 3);
+    if (listen(server_socket, 3) < 0) {
+        report("Listen failed", 1);
+    }
     printf("Waiting for incoming connections...\n");
-    pthread_mutex_init(&lock, NULL);
+    
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        perror("Mutex init failed");
+	    return 1;
+    }
 
-    // Accept incoming connections
-    c = sizeof(struct sockaddr_in);
-    while ((client_socket = accept(server_socket, (struct sockaddr *)&client, (socklen_t*)&c))) {
-        printf("Connection accepted from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    // Initialize variables to accept
+    struct sockaddr_in caddr;   // client address
+    int len_c = sizeof(struct sockaddr_in);
+    int client_socket;
+    int *new_sock;
+    
+    // Accept client connection
+    while ((client_socket = accept(server_socket, (struct sockaddr *)&caddr, (socklen_t*)&len_c))) {
+        printf("Connection accepted from %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
         pthread_t thread_id;
-        new_sock = malloc(1);
+        new_sock = malloc(sizeof(int));
+	if (new_sock == NULL) {
+	    report("Malloc failed", 0);
+	    continue;
+	}
         *new_sock = client_socket;
         pthread_mutex_lock(&lock);
         
@@ -110,14 +145,13 @@ int main(int argc, char *argv[]) {
 
         // Create a new thread for the client
         if (pthread_create(&thread_id, NULL, client_handler, (void*)&client_socket) < 0) {
-            perror("Could not create thread");
-            return 1;
+            report("Could not create thread", 1);
         }
     }
-
+    
+    // Clean up
     if (client_socket < 0) {
-        perror("Accept Failed);
-        return 1;
+        report("Accept Failed", 0);
     }
 
     pthread_mutex_destroy(&lock);
